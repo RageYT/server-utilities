39bd10e196741635b16735e8904c9135ed41b90c
Rage_YouTube <upidevteam@gmail.com>
2019-02-03 00:20:05 -0500

    Fixed ACL Support On Commands
    Fixed ACL support for both server commands, allowing ace permissions.

--- server/commands.lua
+++ server/commands.lua
@@ -4,10 +4,10 @@ local prefix = config.prefix
 RegisterCommand('weapon', function(source, args, raw)
     if args[1] == nil then return end
     TriggerClientEvent('server-utilities:giveWeapon', source, args[1])
-end)
+end, true)
 
 -- Server sided command, provides ace support
 RegisterCommand("vehicle", function(source, args, raw)
     if args[1] == nil then return end
     TriggerClientEvent("server-utilities:spawnVehicle", source, args[1])
-end, false)
+end, true)
